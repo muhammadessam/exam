dc7a4715a034ec6dfae096254144931a6a1bb93e
Muhammad Essam <mohammedessam699@gmail.com>
2020-06-17 02:44:07 +0200

    adds to the ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Exam
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Groups</a>
+                    <a href="https://laracasts.com">Sections</a>
+                    <a href="https://laravel-news.com">Chart</a>
+                    <a href="https://blog.laravel.com">Automatic Result</a>
                 </div>
             </div>
         </div>
