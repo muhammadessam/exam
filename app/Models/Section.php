201d7f11de59eb51699c592fed15c218479213de
Muhammad Essam <mohammedessam699@gmail.com>
2020-06-20 03:58:15 +0200

    adds to the dashboard

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -10,4 +10,16 @@ class Section extends Model
     {
         return $this->hasMany(Group::class, 'section_id', 'id');
     }
+
+    public static function readingSection()
+    {
+        return Section::where('name', 'Reading')->first();
+    }
+
+    public static function listeningSection(){
+        return Section::where('name', 'Listening')->first();
+    }
+    public static function lsSection(){
+        return Section::where('name', 'Language System')->first();
+    }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Reading Groups</h1>
+                    <h1 class="m-0 text-dark">Dashboard</h1>
                 </div>
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -17,7 +17,45 @@
         <div class="container-fluid">
             <div class="card">
                 <div class="card-body">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>{{\App\Models\Section::readingSection()->groups()->count()}}</h3>
+                                    <p>Reading Section Groups</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="fa fa-book"></i>
+                                </div>
+                                <a href="{{route('admin.sections.show', \App\Models\Section::readingSection())}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
 
+                        <div class="col-4">
+                            <div class="small-box bg-success">
+                                <div class="inner">
+                                    <h3>{{\App\Models\Section::listeningSection()->groups()->count()}}</h3>
+                                    <p>Reading Section Groups</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="fa fa-headphones"></i>
+                                </div>
+                                <a href="{{route('admin.sections.show', \App\Models\Section::listeningSection())}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="small-box bg-danger">
+                                <div class="inner">
+                                    <h3>{{\App\Models\Section::lsSection()->groups()->count()}}</h3>
+                                    <p>Reading Section Groups</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="fa fa-edit"></i>
+                                </div>
+                                <a href="{{route('admin.sections.show', \App\Models\Section::lsSection())}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div><!-- /.container-fluid -->
